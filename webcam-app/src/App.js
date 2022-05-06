68f8035cbbfb448d5b571af3aba6e520fb75222c
naeem123650 <naeemmemon60650@gmail.com>
2022-05-06 19:18:37 +0530

    first commit

---
+++ webcam-app/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import WebCapture from './components/WebCapture';
+
+function App() {
+  return (
+    <div className="App">
+      <WebCapture />
+    </div>
+  );
+}
+
+export default App;
---
+++ webcam-app/src/components/WebCapture.js
@@ -0,0 +1,34 @@
+
+import React from 'react'
+import Webcam from 'react-webcam'
+
+const WebCapture = () => {
+  const webcamRef = React.useRef(null);
+  const [imgSrc, setImgSrc] = React.useState(null);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef, setImgSrc]);
+
+
+  return (
+    <>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+      />
+      <button onClick={capture}>Capture photo</button>
+      {imgSrc && (
+        <img
+          src={imgSrc}
+          alt="img"
+        />
+      )}
+    </>
+  );
+};
+
+export default WebCapture;
+// https://www.npmjs.com/package/react-webcam
